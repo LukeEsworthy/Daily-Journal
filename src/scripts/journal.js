a7367f3f809c6dda33c4ff4c8539a1dae0103575
Luke Esworthy <lukesworthy@gmail.com>
2020-04-27 14:48:17 -0500

    saving changes so I can practice working on branch

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,14 +1,15 @@
 const journalEntryDate = document.getElementById("journalDate").innerText;
-const journalConceptsCovered = document.getElementById("conceptsCovered").innerText;
+const journalConceptsCovered = document.getElementById("conceptsCovered")
+  .innerText;
 const journalJournalEntry = document.getElementById("journalEntry").innerText;
 const journalMood = document.getElementById("mood").innerText;
 
 const singleJournalEntry = {
-    entryDate: journalEntryDate,
-    conceptsCovered: journalConceptsCovered,
-    journalEntry: journalJournalEntry,
-    mood: journalMood,
-}
+  entryDate: journalEntryDate,
+  conceptsCovered: journalConceptsCovered,
+  journalEntry: journalJournalEntry,
+  mood: journalMood,
+};
 
 const journalEntryCollection = [];
 
