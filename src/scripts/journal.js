98646a799028dac73b2039137e2b7c9c49023ca8
Luke Esworthy <lukesworthy@gmail.com>
2020-04-27 15:06:43 -0500

    added journal entries

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -15,4 +15,22 @@ const journalEntryCollection = [];
 
 journalEntryCollection.push(singleJournalEntry);
 
+const extraJournalEntry1 = {
+  date: "April 27, 2020",
+  concept: "APIs",
+  entry:
+    "Today we covered using external APIs, Postman, and the importance of .gitignore.",
+  mood: "Ok, could be better",
+};
+
+const extraJournalEntry2 = {
+  date: "April 27, 2020",
+  concept: "John Wark talk",
+  entry:
+    "Today we had a talk by NSS CEO John Wark. He went over the types of businesses that hire NSS grads and average/expected salaries upon graduation from NSS bootcamp.",
+  mood: "Encouraged",
+};
+
+journalEntryCollection.push(extraJournalEntry1, extraJournalEntry2);
+
 console.log(journalEntryCollection);
