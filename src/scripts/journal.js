0a87b824faa620915b23cbbb2092edb52db34a38
Luke Esworthy <lukesworthy@gmail.com>
2020-05-05 10:36:41 -0500

    daily journal part 4

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -15,22 +15,8 @@ const journalEntryCollection = [];
 
 journalEntryCollection.push(singleJournalEntry);
 
-const extraJournalEntry1 = {
-  date: "April 27, 2020",
-  concept: "APIs",
-  entry:
-    "Today we covered using external APIs, Postman, and the importance of .gitignore.",
-  mood: "Ok, could be better",
-};
-
-const extraJournalEntry2 = {
-  date: "April 27, 2020",
-  concept: "John Wark talk",
-  entry:
-    "Today we had a talk by NSS CEO John Wark. He went over the types of businesses that hire NSS grads and average/expected salaries upon graduation from NSS bootcamp.",
-  mood: "Encouraged",
-};
-
-journalEntryCollection.push(extraJournalEntry1, extraJournalEntry2);
-
-console.log(journalEntryCollection);
+fetch("http://localhost:3000/entries")
+  .then((entriesResults) => entriesResults.json())
+  .then((entries) => {
+    console.log(entries);
+  });
